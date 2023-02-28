320cd487cff4c5242f3f6276e4206a6c8472480e
HarryVu176 <harry@moonivia.com>
2023-02-28 08:26:23 -0500

    Upload something

---
+++ Leetcode/ok.py
@@ -0,0 +1,8 @@
+class Solution:
+    def leftRigthDifference(self, nums) -> list[int]:
+        return 0
+    def main(self):
+        print(self.leftRigthDifference(123))
+
+if __name__ == "__main__":
+    Solution().main()
